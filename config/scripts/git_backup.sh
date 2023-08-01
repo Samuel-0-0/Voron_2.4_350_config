e236419972d135727d5d7a0481c30dbfba0c69be
Samuel Wang <imhsaw@gmail.com>
2023-08-01 09:56:53 +0800

    [Klipper:d725dfd,Moonraker:f487de7,Mainsail:v2.6.2]

--- config/scripts/git_backup.sh
+++ config/scripts/git_backup.sh
@@ -2,16 +2,14 @@
 
 ### 需要备份的文件夹
 config_folder=~/printer_data
-
 ### Klipper文件夹位置，默认'~/klipper'
 klipper_folder=~/klipper
-
 ### Moonraker文件夹位置，默认'~/moonraker'
 moonraker_folder=~/moonraker
-
 ### Mainsail文件夹位置，默认'~/mainsail'
 mainsail_folder=~/mainsail
 
+### 获取版本信息
 grab_version(){
   if [ ! -z "$klipper_folder" ]; then
     echo -n "Getting klipper version="
@@ -37,18 +35,21 @@ grab_version(){
   fi
 }
 
+### 配置备份操作
 push_config(){
   cd $config_folder
-  echo Pushing updates ...
+  echo 拉取远程更新 ...
   #从远程仓库拉取更新
   git pull -v
+  echo 与本地更新合并 ...
   #合并
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   read -p "更新说明:" babala
-  git commit -m "$babala[$m1,$m2,$m3]"
+  git commit -m "$babala [$m1,$m2,$m3]"
   git push
 }
 
+### 执行
 grab_version
 push_config
