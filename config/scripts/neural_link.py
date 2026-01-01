20adb40163be027a2149c4827353fdde41110571
Samuel Wang <imhsaw@gmail.com>
2026-01-01 12:39:19 +0800

    更换ws端口 [Klipper:dd62593,Moonraker:5b92e52,Mainsail:v2.16.1]

--- config/scripts/neural_link.py
+++ config/scripts/neural_link.py
@@ -285,4 +285,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8765)
+    uvicorn.run(app, host="0.0.0.0", port=1111)
